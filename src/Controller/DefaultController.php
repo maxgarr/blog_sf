b199c02461326160970a379ff31d0fd8a78c3c1e
maxgarr <maxime.garron17@gmail.com>
2022-03-18 17:24:59 +0100

    v1

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\Expr\Value;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]
-    public function index(): Response
+    #[Route('/', name: 'accueil')]
+    public function homePage(): Response
+    {        
+        return $this->render('default/index.html.twig');
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contactPage(): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+        return $this->render('default/contact.html.twig');
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function adminPage(): Response
+    {
+        return $this->render('admin/admin.html.twig');
+    }
+
+    #[Route('/blog', name: 'blog', methods: ['GET'])]
+    public function blogPage(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('default/blog.html.twig',[
+            'articles' => $articleRepository->findAll(),
         ]);
     }
 }
---
+++ src/Entity/Article.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $auteur;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $titre;
+
+    #[ORM\Column(type: 'text', length: 255)]
+    private $contenu;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $createdAt;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $image;
+
+    #[ORM\Column(type: 'datetime_immutable', nullable: true)]
+    private $updateAt;
+
+    public function __construct()
+    {
+        
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    #[ORM\PrePersist]
+    public function setCreatedAt(): self
+    {
+        $this->createdAt = new DateTimeImmutable();
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeImmutable
+    {
+        return $this->updateAt;
+    }
+
+    #[ORM\PreUpdate]
+    public function setUpdateAt(): self
+    {
+        $this->updateAt = new DateTimeImmutable();
+
+        return $this;
+    }
+}
